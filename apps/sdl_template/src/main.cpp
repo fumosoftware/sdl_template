23cd5a507cf0b29f5b6745c536a0f6e5e20893e4
fumosoft <projectacorn@fumosoft.com>
2024-10-27 00:25:30 -0500

    modernized with std::except

--- apps/sdl_template/src/main.cpp
+++ apps/sdl_template/src/main.cpp
@@ -3,25 +3,27 @@
 //
 
 #include <iostream>
+#include <expected>
 #include "SDL3/SDL.h"
 
-int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
-  if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != true) {
-    return EXIT_FAILURE;
-  }
+struct Context {
+  SDL_Window* window{nullptr};
+  SDL_Renderer* renderer{nullptr};
+};
 
-  SDL_Window* window = SDL_CreateWindow("sdl_template", 600, 380, 0);
-  if(window == nullptr) {
-    SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
-  SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
-  if(renderer == nullptr) {
-    SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
-    SDL_Quit();
-    return EXIT_FAILURE;
-  }
+struct Error {
+  std::string reason{};
+};
+
+std::expected<Context, Error> initialize();
+void render(Context const& context);
+void shutdown_sdl(Context const& context);
+
+int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
+
+  auto maybe_context = initialize();
+  if(!maybe_context) return EXIT_FAILURE;
+  auto context = *maybe_context;
 
   bool is_running = true;
   SDL_Event event{};
@@ -32,12 +34,52 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
       }
     }
 
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
+    render(context);
   }
 
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
+  shutdown_sdl(context);
   return EXIT_SUCCESS;
+}
+
+std::expected<void, Error> initialize_sdl() {
+  if(SDL_WasInit(0) == 0) {
+    return std::unexpected{Error{"SDL was already initialized!"}};
+  }
+
+  if(!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+    return std::unexpected{Error{SDL_GetError()}};
+  }
+
+  return {};
+}
+
+std::expected<Context, Error> create_context() {
+  Context context;
+  if(!SDL_CreateWindowAndRenderer("sdl_template", 600, 380, 0, &context.window, &context.renderer)) {
+    return std::unexpected{Error{SDL_GetError()}};
+  }
+
+  return context;
+}
+
+std::expected<Context, Error> quit_sdl(Error const& error) {
+  SDL_Quit();
+  return std::unexpected{error};
+}
+
+std::expected<Context, Error> initialize() {
+  return initialize_sdl()
+          .and_then(create_context)
+          .or_else(quit_sdl);
+}
+
+void render(Context const& context) {
+  SDL_RenderClear(context.renderer);
+  SDL_RenderPresent(context.renderer);
+}
+
+void shutdown_sdl(Context const& context) {
+  SDL_DestroyRenderer(context.renderer);
+  SDL_DestroyWindow(context.window);
+  SDL_Quit();
 }
