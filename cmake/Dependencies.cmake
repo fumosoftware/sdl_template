48a8e61441ae09cc7bdc566add835004be7f20ad
fumosoft <projectacorn@fumosoft.com>
2024-11-01 00:48:58 -0500

    removed sdl3 until it is officially released

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,9 +1,5 @@
 include(CPM)
 
 CPMAddPackage("gh:fmtlib/fmt#10.2.1")
-if(${sdl_template_USE_SDL3})
-    CPMAddPackage("gh:libsdl-org/sdl#preview-3.1.3")
-else ()
-    CPMAddPackage("gh:libsdl-org/sdl#release-2.30.8")
-    CPMAddPackage("gh:libsdl-org/sdl_image#release-2.8.2")
-endif()
+CPMAddPackage("gh:libsdl-org/sdl#release-2.30.8")
+CPMAddPackage("gh:libsdl-org/sdl_image#release-2.8.2")
--- cmake/ProjectOptions.cmake
+++ cmake/ProjectOptions.cmake
@@ -1,4 +1,3 @@
 
 function(SetupProjectOptions)
-    option(sdl_template_USE_SDL3 "Build with SDL3 instead of SDL2" OFF)
 endfunction()
